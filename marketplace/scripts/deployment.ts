bc7509ffcdc3b53e690cb55c06ceba84131057e4
GideonBature <infoaboutgideon@gmail.com>
2024-08-31 00:09:27 +0100

    declare addresses, account and provider

--- marketplace/scripts/deployment.ts
+++ marketplace/scripts/deployment.ts
@@ -2,3 +2,9 @@ import { Account, RpcProvider, Contract, json, CallData, uint256, Call, Calldata
 import fs from 'fs';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+const ethAddress = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+const starkAddress = "0x0307784111703d85B35Ff9542ED0b9FB959aBBe193e12662D079715D2C1c1864"
+
+let provider: RpcProvider;
+let account: Account;
